6ea67f9ba131ff172cfcf03f34209d5ccf1beda4
actokuyt <chibthankgod@gamil.com>
2024-08-10 06:43:22 +0100

    minor fix 8.2

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,5 +62,5 @@ module "ec2" {
   key_name      = "ec2ssh"
   public_sg_id  = module.sg.public_sg_id
   public_subnet_id = module.subnet.subnet_id
-  private_key_path = "ec2ssh.pem"
+  private_key_path = var.private_key_path
 }
